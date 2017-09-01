a0d90aa8852275862f1b21babf0b34731c597390
wangrunmin <272295451@qq.com>
2017-09-02 00:28:23 +0800

    完成用户绑定微信

--- pages/register/register.js
+++ pages/register/register.js
@@ -4,31 +4,23 @@ Page({
     userInfo: {}
   },
 
-  onLoad: function () {
-    var that = this;
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'http://localhost:6407/api/wechat/1',
-            data: {
-              code: res.code
-            },
-            success: function (res) {
-              if (res.data == "新用户") {
-
-              }
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    });
+  onLoad: function (option) {
+    var res=JSON.parse(option.res);
+    this.setData({
+      userInfo:res,
+    })
+    console.log(res);
   },
   formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.request({
+      url: 'http://localhost:61537/api/user/save',
+      data:{
+        data: e.detail.value
+      },
+      success:function(res){
+        alert(res.data)
+      }
+    });
   },
   formReset: function () {
     console.log('form发生了reset事件')
